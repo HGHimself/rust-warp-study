1226800b7d966e300e99083c6d5d501f591f0861
hgm-king <hgmaxwellking@gmail.com>
2023-07-16 10:48:22 -0400

    format

--- src/handlers/page.rs
+++ src/handlers/page.rs
@@ -55,17 +55,15 @@ pub fn process_page_error(
     resource: &ResourceErrorData,
     message: &str,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    if let Some(expanded_user) = resource.expanded_user.clone() 
-    && let Some(expanded_page) = resource.expanded_page.clone() 
-    && let Some(context) = resource.context.clone() {
-        
+    if let Some(expanded_user) = resource.expanded_user.clone() && let Some(expanded_page) = resource.expanded_page.clone() && let Some(context) = resource.context.clone() {
+
         let links = get_links(context, &expanded_page)?;
 
         let html = views::page::view_authenticated(
-            expanded_user, 
-            expanded_page, 
-            links, 
-            "Error: Link already exists in this page"
+            expanded_user,
+            expanded_page,
+            links,
+            message
         );
 
         error_reply(StatusCode::CONFLICT, html)
@@ -78,22 +76,21 @@ pub fn process_profile_error(
     resource: &ResourceErrorData,
     message: &str,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    if let Some(expanded_user) = resource.expanded_user.clone() 
-        && let Some(context) = resource.context.clone() {
-            let pages = handlers::user::get_pages(context, &expanded_user)?;
-            let html = views::user::profile(
-                expanded_user.user,
-                expanded_user.background,
-                pages,
-                "Error: Page already exists with this name",
-            );
-            error_reply(StatusCode::CONFLICT, html)
-        } else {
-            error_reply(
-                StatusCode::INTERNAL_SERVER_ERROR,
-                views::error::error(StatusCode::INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR"),
-            )
-        }
+    if let Some(expanded_user) = resource.expanded_user.clone() && let Some(context) = resource.context.clone() {
+        let pages = handlers::user::get_pages(context, &expanded_user)?;
+        let html = views::user::profile(
+            expanded_user.user,
+            expanded_user.background,
+            pages,
+            message,
+        );
+        error_reply(StatusCode::CONFLICT, html)
+    } else {
+        error_reply(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            views::error::error(StatusCode::INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR"),
+        )
+    }
 }
 
 fn get_links(
